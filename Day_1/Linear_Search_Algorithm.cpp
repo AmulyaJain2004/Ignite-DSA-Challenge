9af32034839943b871211f97dae3a7863574eea7
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-12-22 23:29:42 +0530

    Update Linear_Search_Algorithm.cpp
    Program to implement linear search algorithm.
    Linear Search is defined as a sequential search algorithm that starts at one end and goes through each element of a list until the desired element is found, otherwise the search continues till the end of the data set. (source for definition: GeeksforGeeks)

--- Day_1/Linear_Search_Algorithm.cpp
+++ Day_1/Linear_Search_Algorithm.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
 
+int linear_search(int* par, int size_arr, int target)
+{
+    for (int i = 0; i < size_arr; i++)
+    {
+
+        if (*(par + i) == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {19, 45, 52, 21, 76, 98, 65, 1, 39};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int search;
+    cout << "Enter the element to search in Array: -" << endl;
+    cin >> search;
+
+    int found = linear_search(arr, size, search);
+
+    if (found == -1)
+    {
+        cout << "Element not found in the array" << endl;
+    }
+    else
+    {
+        cout << "Element found in the array and at the index " << found << endl;
+    }
+
+    return 0;
+}
