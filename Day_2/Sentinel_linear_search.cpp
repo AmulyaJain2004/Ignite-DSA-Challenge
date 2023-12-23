5b2cbfe30ab6eb08f9dcbe5abe15ce5272d7f74f
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-12-23 23:14:13 +0530

    Update Sentinel_linear_search.cpp
    Program to implement Sentinel Linear Search.
    Sentinel Linear search is a type of linear search where the element to be searched is placed in the last position and then all the indices are checked for the presence of the element without checking for the index out of bound case. (source for definition GeeksforGeeks).
    Its a better approach as compared to normal linear search as number to operations greatly reduced from number 2*n+1 to n+2 in  worst case.

--- Day_2/Sentinel_linear_search.cpp
+++ Day_2/Sentinel_linear_search.cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+using namespace std;
 
+int Sentinel_Linear_Search(int arr[], int size_arr, int search)
+{
+    // temporary storing last element of array in a variable
+    int end_term = arr[size_arr - 1];
+
+    // assigning last element value = element to be searched
+    arr[size_arr - 1] = search;
+
+    // linear search that is traversing through the whole array till we found the element and incrementing i
+    int i = 0;
+    while (arr[i] != search)
+    {
+        i++;
+    }
+
+    // putting back the original value of last element of array
+    arr[size_arr - 1] = end_term;
+
+    // checking if the target element is present before the last element or last element of array is the element we are searching for
+    if (i < size_arr - 1 || arr[size_arr - 1] == search)
+    {
+        return i;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    int arr[] = {19, 45, 52, 21, 76, 98, 65, 1, 39};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int target;
+    cout << "Enter the element to search in Array: -" << endl;
+    cin >> target;
+
+    int found = Sentinel_Linear_Search(arr, size, target);
+
+    if (found == -1)
+    {
+        cout << "Element not found in the array" << endl;
+    }
+    else
+    {
+        cout << "Element found in the array and at the index " << found << endl;
+    }
+    
+    return 0;
+}
