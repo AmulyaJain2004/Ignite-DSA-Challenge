3261992e7081b26e20c9ecccd48e5d0ccba875db
Amulya Jain <jainamulyawin@gmail.com>
2025-01-11 11:19:37 +0530

    Added codes

---
+++ Step-01-Learn-the-basics/Lec-03/01-CppSTL.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h> // includes all the library and utilities of c++ using one line
+using namespace std; // useful when we don't want to not write always std:: before cout and cin
+
+void print()
+{
+    cout << "Amulya";
+    return;
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+    print();
+    cout << a;
+    return 0;
+}
